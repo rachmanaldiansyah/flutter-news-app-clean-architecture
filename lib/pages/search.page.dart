aa5a1ffa8d43c92756dfbf26009b1458a24223d9
RACHMAN21 <rachmanaldiansyah03@gmail.com>
2023-12-27 16:40:11 +0700

    feat: menambah fitur pencarian berita yang ingin dicari

--- lib/pages/search.page.dart
+++ lib/pages/search.page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_clean_architecture/components/news.component.dart';
+import 'package:news_app_clean_architecture/providers/news.provider.dart';
+import 'package:provider/provider.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,19 +11,64 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController(text: '');
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Berita yang Anda cari'),
-      ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Ini adalah berita yang dicari....'),
-          ],
+    return Consumer<NewsProvider>(
+        builder: (BuildContext context, news, Widget? child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Berita yang Anda cari'),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: searchController,
+                        decoration: const InputDecoration(
+                          hintText: 'Cari berita...',
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        news.getSearch(searchController.text);
+                      },
+                      icon: const Icon(Icons.send),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                news.isDataEmpty
+                    ? const SizedBox()
+                    : news.isLoadingSearch
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Column(
+                            children: [
+                              ...news.resSearch!.articles!.map(
+                                (e) => News(
+                                  title: e.title ?? "",
+                                  image: e.urlToImage ?? "",
+                                ),
+                              )
+                            ],
+                          )
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
--- lib/providers/news.provider.dart
+++ lib/providers/news.provider.dart
@@ -5,8 +5,12 @@ import 'package:news_app_clean_architecture/models/topNews.model.dart';
 import '../utils/const.dart';
 
 class NewsProvider with ChangeNotifier {
+  bool isDataEmpty = true;
   bool isLoading = true;
+  bool isLoadingSearch = true;
+
   TopNewsModel? resNews;
+  TopNewsModel? resSearch;
 
   setLoading(data) {
     isLoading = data;
@@ -14,7 +18,7 @@ class NewsProvider with ChangeNotifier {
   }
 
   getTopNews() async {
-    final res = await api('${baseUrl}top-headlines?country=us&apiKey=$apiKey');
+    final res = await api('${baseUrl}top-headlines?country=id&apiKey=$apiKey');
 
     if (res.statusCode == 200) {
       resNews = TopNewsModel.fromJson(res.data);
@@ -22,6 +26,23 @@ class NewsProvider with ChangeNotifier {
       resNews = TopNewsModel();
     }
     isLoading = false;
+    notifyListeners();
+  }
+
+  getSearch(String search) async {
+    isDataEmpty = false;
+    isLoadingSearch = true;
+    notifyListeners();
+
+    final res = await api(
+        '${baseUrl}everything?q=$search&from=2023-12-26&to=2023-12-26&sortBy=popularity&apiKey=$apiKey');
+
+    if (res.statusCode == 200) {
+      resSearch = TopNewsModel.fromJson(res.data);
+    } else {
+      resSearch = TopNewsModel();
+    }
+    isLoadingSearch = false;
     notifyListeners();
   }
 }
